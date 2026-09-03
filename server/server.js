d69e146dbfa1a614b1c901d1e560ec603217a1e9
Fatima Khan <fatemahkhan639@gmail.com>
2026-09-03 17:00:37 -0400

    Fix CORS origin to match actual Vercel domain

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://velour-j964lldrl-fatemah-khan.vercel.app"]
+  origin: ["http://localhost:5173", "https://velour-app-seven.vercel.app"]
 }));
 app.use(express.json());
 

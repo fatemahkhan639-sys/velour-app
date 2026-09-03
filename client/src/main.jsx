a68c5d736f2c496d7bfef3f8fa44c6e0137f5780
Fatima Khan <fatemahkhan639@gmail.com>
2026-09-03 16:45:56 -0400

    Add axios base URL config for production API calls

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,9 @@ import { CartProvider } from "./context/CartContext";
 import App from "./App";
 import "./index.css";
 
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
